12b3f9aea262881a9e889b41253d84604198a9f2
Time <timejim20205@gmail.com>
2026-03-12 05:14:56 +0000

    完成“弱监督学习”的初步构建

--- 番外：训练方法/weaky_SL.py
+++ 番外：训练方法/weaky_SL.py
@@ -4,6 +4,11 @@ from mlp import MLP
 dataType = np.float16  # 设置存储和计算时使用的数据类型，可以节约内存，并提高计算速度
 
 
+def normal(x, min, max):
+    norm = (x - min) / (max - min)
+    return norm
+
+
 def build_data(dataType, min, max):
     '''
     生成未标注的数据
@@ -11,7 +16,6 @@ def build_data(dataType, min, max):
     x_0 = np.random.uniform(min, max, size=(
         100, 1)).astype(dataType)  # 构建异或为0的数据
     x_0 = x_0 + np.zeros((x_0.shape[0], 2), dtype=dataType)
-    x_0 = x_0 / (max - min)  # 输入数据归一化
 
     # 构建异或为1的数据(由于有去重和筛选环节，因此需要留出多余的数来确保经过这些操作后数据能到达和x_0同等规模)
     x_1 = np.random.uniform(min, max, size=(
@@ -20,7 +24,9 @@ def build_data(dataType, min, max):
     mask = x_1[:, 0] != x_1[:, 1]
     x_1 = x_1[mask]  # 取两个数不同的数组
     x_1 = np.unique(x_1, axis=0)
-    x_1 = x_1 / np.max(x_1)
+
+    x_0 = normal(x_0, min, max)
+    x_1 = normal(x_1, min, max)
 
     x = np.concatenate((x_1, x_0), axis=0)  # 沿着行的方向合并数据
     index = np.random.permutation(x.shape[0])
@@ -29,7 +35,7 @@ def build_data(dataType, min, max):
     return x
 
 
-def p_to_l(label_p: np.ndarray, top_k: int, min_p: float, max_p: float):
+def p_to_l(label_p: np.ndarray, top_k: int, min_p: float):
     '''
     将预测概率转换为伪标签
     '''
@@ -43,15 +49,12 @@ def p_to_l(label_p: np.ndarray, top_k: int, min_p: float, max_p: float):
     for cls in cls_index:
         cls_eye = np.zeros(label_p.shape[0], dtype=np.int64) + cls
         cls_eye = np.eye(label_p.shape[1])[cls_eye]
-        cls_mask = np.where((cls_eye == 1), True, False)
+        cls_mask = (cls_eye == 1)
 
         cls_p = label_p[cls_mask]
         sorted_cls_p_index = np.argsort(cls_p)[::-1]  # 由大到小排序
 
-        sorted_label_p = label_p[sorted_cls_p_index]
-        min_p_mask = np.where(cls_p > min_p, True, False)
-        max_p_mask = np.where(cls_p < max_p, True, False)
-        keep_mask = np.where(max_p_mask == min_p_mask, True, False)
+        keep_mask = (cls_p > min_p)
 
         label_masked_index = sorted_cls_p_index[keep_mask]
         label_masked = cls_eye[keep_mask]
@@ -62,7 +65,7 @@ def p_to_l(label_p: np.ndarray, top_k: int, min_p: float, max_p: float):
     return np.array(label, dtype=dataType), np.array(index[:top_k], dtype=np.int64)
 
 
-def weaky_sl(model: MLP, basedata_x: np.ndarray, basedata_y: np.ndarray, unlabel_data: np.ndarray, lr: float, top_k: int, min_p: float, max_p: float):
+def weaky_sl(model: MLP, basedata_x: np.ndarray, basedata_y: np.ndarray, unlabel_data: np.ndarray, lr: float, top_k: int, min_p: float):
     model.train(basedata_x, basedata_y, lr=1e-2, epoch=1000, note_step=10)
     print("预训练完成，进行标注")
 
@@ -73,7 +76,7 @@ def weaky_sl(model: MLP, basedata_x: np.ndarray, basedata_y: np.ndarray, unlabel
     for i in range(step):
         fake_label_p = model.forward(unlabel_data)
         fake_label, fake_label_index = p_to_l(
-            fake_label_p, top_k, min_p, max_p)
+            fake_label_p, top_k, min_p)
 
         if fake_label.size != 0:
             unlabel_x = unlabel_data[fake_label_index].tolist()
@@ -121,7 +124,7 @@ if __name__ == "__main__":
     mlp = MLP([input_dim, hide_dim, output_dim], dtype=dataType)
 
     mlp = weaky_sl(mlp, base_data_x, base_data_y,
-                   unlabel_x, 1e-4, 4, 0.0, 1.0)
+                   unlabel_x, 1e-4, 4, 0.6)
 
     x_test = np.array([
         [2, 3],
@@ -129,7 +132,7 @@ if __name__ == "__main__":
         [4, 5],
         [5, 5]
     ]).astype(dataType)
-    x_test = x_test / np.max(x_test)
+    x_test = (x_test - x_test.min()) / (x_test.max() - x_test.min())
 
     y_test = np.array([
         [0, 1],
